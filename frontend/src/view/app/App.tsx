8fa74aedbf4e19d53d581d5a67a9bffcb9d09de0
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-03-29 01:09:44 +0800

    fix: 放弃mdbox

--- frontend/src/view/app/App.tsx
+++ frontend/src/view/app/App.tsx
@@ -6,16 +6,12 @@ import enUS from "antd-mobile/es/locales/en-US";
 import { ConfigProvider } from "antd-mobile";
 import { useI18nStore } from "@/store/i18n.ts";
 import zhCN from "antd-mobile/es/locales/zh-CN";
-import { initJsBridge } from '@/util/jsBridge';
-import { useAsyncEffect } from 'ahooks';
-import { usePersonStore } from '@/store/config';
 
 function App() {
     configure({
         axios: axiosInstance
     })
     const i18nStore = useI18nStore();
-    const { setEnv } = usePersonStore()
 
     // useAsyncEffect(async () => {
     //     await initJsBridge();
