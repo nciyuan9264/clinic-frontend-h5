8d48039b28c5b709b585e15de907fe9fbeb68505
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-03-28 22:48:53 +0800

    fix: 编译为低版本

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,21 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [react() ],
-    base: '/h5/', // 让静态资源路径加上 /h5/
-    server: {
-        port: 80,
-        host: '0.0.0.0',
+  plugins: [react()],
+  base: '/h5/', // 让静态资源路径加上 /h5/
+  server: {
+    port: 80,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '~': '/src',
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '~': '/src',
-        }
-    }
-})
+  },
+  build: {
+    target: 'es2020', // 降级 JS 目标版本
+  },
+});
